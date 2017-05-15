1ddb1f0d413931bf42bdd46f74b31d9d5135eca3
Nash Kaminski <nashkaminski@gmail.com>
2017-05-15 17:41:44 -0500

    initial mqtt tinkering

---
+++ mqtt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import paho.mqtt.client as mqtt
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+# If you want to use a specific client id, use
+# mqttc = mqtt.Client("client-id")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+mqttc.connect("localhost", 1883, 60)
+mqttc.subscribe("mqtt/light/#", 0)
+
+
+mqttc.loop_forever()
+
